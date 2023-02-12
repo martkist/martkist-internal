918c7adb606a1a597c824c27907a15cafa75b01a
blockmartkist <blockmartkist@protonmail.com>
2023-02-12 13:23:31 +0200

    Switch to QtWebKit

--- src/qt/freechpage.cpp
+++ src/qt/freechpage.cpp
@@ -10,13 +10,41 @@
 #include "wallet/wallet.h"
 #include "walletmodel.h"
 
+#include <QtWebEngineWidgets/QWebEnginePage>
+#include <QNetworkCookieJar>
+
+class FreechWebPage : public QWebPage
+{
+public:
+    FreechWebPage(QObject* parent = 0) : QWebPage(parent) {}
+    virtual void javaScriptConsoleMessage(const QString& message, int lineNumber, const QString& sourceID)
+    {
+        LogPrintf("Freech::Javascript %s (%s:%i)\n", qPrintable(message), qPrintable(sourceID), lineNumber);
+    }
+};
+
 FreechPage::FreechPage(const PlatformStyle *platformStyle, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::FreechPage),
     clientModel(0),
     walletModel(0)
-{
+{    
+    QWebSettings* settings = QWebSettings::globalSettings();
+    settings->setAttribute(QWebSettings::LocalStorageEnabled, true);
+    auto path = QString::fromStdString((GetDataDir() / "freech").string());
+    settings->setOfflineStoragePath(path);
+    settings->enablePersistentStorage(path);
+
     ui->setupUi(this);
+
+    QUrl url = QUrl(QString::fromUtf8("http://localhost:4032/"));
+    url.setUserName("user");
+    url.setPassword("pwd");
+
+    FreechWebPage *page = new FreechWebPage(ui->webView);
+    ui->webView->setPage(page);
+    ui->webView->load(url);
+    ui->webView->show();
 }
 
 FreechPage::~FreechPage()
