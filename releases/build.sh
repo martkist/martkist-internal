3d35421678f5dfa2aa071f4729f15dac06ad5255
blockmartkist <blockmartkist@protonmail.com>
2023-02-23 23:26:25 +0200

    build fixes

--- releases/build.sh
+++ releases/build.sh
@@ -4,13 +4,13 @@ DOCKEROPTS="--build-arg TAG=$MARTKIST_TAG --build-arg MAKEOPTS=$MAKEOPTS"
 # echo "Building Linux release..."
 # docker build $DOCKEROPTS -t martkist:release-linux-$MARTKIST_TAG -f release-linux.Dockerfile . 
 # container_id=$(docker create "martkist:release-linux-$MARTKIST_TAG")
-# docker cp "$container_id:/outputs" .
+# docker cp -q "$container_id:/outputs" .
 # docker rm "$container_id"
 
 echo "Building Windows release..."
 DOCKER_BUILDKIT=0 docker build $DOCKEROPTS -t martkist:release-win-$MARTKIST_TAG -f release-win.Dockerfile .
 container_id=$(docker create "martkist:release-win-$MARTKIST_TAG")
-docker cp "$container_id:/outputs/" .
+docker cp -q "$container_id:/outputs/" .
 docker rm "$container_id"
 
 # echo "Building macOS release..."
