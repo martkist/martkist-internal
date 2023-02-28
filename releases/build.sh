d7504d2f03f1c2260883e274b39d915c3a026e8a
blockmartkist <blockmartkist@protonmail.com>
2023-02-28 08:24:01 +0200

    re-enable all builds

--- releases/build.sh
+++ releases/build.sh
@@ -7,16 +7,16 @@ container_id=$(docker create "martkist:release-linux-$MARTKIST_TAG")
 docker cp -q "$container_id:/outputs" .
 docker rm "$container_id"
 
-# echo "Building Windows release..."
-# DOCKER_BUILDKIT=0 docker build $DOCKEROPTS -t martkist:release-win-$MARTKIST_TAG -f win/release-win.Dockerfile .
-# container_id=$(docker create "martkist:release-win-$MARTKIST_TAG")
-# docker cp -q "$container_id:/outputs/" .
-# docker rm "$container_id"
+echo "Building Windows release..."
+DOCKER_BUILDKIT=0 docker build $DOCKEROPTS -t martkist:release-win-$MARTKIST_TAG -f win/release-win.Dockerfile .
+container_id=$(docker create "martkist:release-win-$MARTKIST_TAG")
+docker cp -q "$container_id:/outputs/" .
+docker rm "$container_id"
 
-# echo "Building macOS release..."
-# DOCKER_BUILDKIT=0 docker build $DOCKEROPTS -t martkist:release-macos-$MARTKIST_TAG -f macos/release-macos.Dockerfile .
-# container_id=$(docker create "martkist:release-macos-$MARTKIST_TAG")
-# docker cp -q "$container_id:/outputs/" .
-# docker rm "$container_id"
+echo "Building macOS release..."
+DOCKER_BUILDKIT=0 docker build $DOCKEROPTS -t martkist:release-macos-$MARTKIST_TAG -f macos/release-macos.Dockerfile .
+container_id=$(docker create "martkist:release-macos-$MARTKIST_TAG")
+docker cp -q "$container_id:/outputs/" .
+docker rm "$container_id"
 
 echo "Done!"
